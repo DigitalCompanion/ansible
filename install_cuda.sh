c3d79f3467edc7f9f835986a35e8278b1a38392e
Fredrik Hansen <46652156+DigitalCompanion@users.noreply.github.com>
2023-04-18 15:06:56 +0200

    Added docker-compose

--- install_cuda.sh
+++ install_cuda.sh
@@ -1,8 +1,10 @@
 sudo wget -O /etc/apt/preferences.d/cuda-repository-pin-600 https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
 sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.0-1_all.deb 
+### Install GPG keys
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.0-1_all.deb
 sudo dpkg -i cuda-keyring_1.0-1_all.deb
+
 
 sudo apt-key del 7fa2af80
 
@@ -28,6 +30,9 @@ sudo apt-mark hold nvidia-cuda-toolkit
 sudo apt-mark hold nvidia-container-toolkit 
 sudo apt-mark hold nvidia-container-runtime
 
-### Install GPG keys
-#wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.0-1_all.deb
-#sudo dpkg -i cuda-keyring_1.0-1_all.deb
+### Install docker-compose
+
+VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | grep -Po '"tag_name": "\K.*\d')
+DESTINATION=/usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
+sudo chmod 755 $DESTINATION
