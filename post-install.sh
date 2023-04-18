fb95aa87638bb754d3abb207031e7346f55a19b2
Fredrik Hansen <46652156+DigitalCompanion@users.noreply.github.com>
2023-04-18 14:53:39 +0200

    added disabling of power save services not needed on a virtual/server

--- post-install.sh
+++ post-install.sh
@@ -1,2 +1,3 @@
+sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
 sudo lvextend --extents +100%FREE /dev/mapper/ubuntu--vg-ubuntu--lv
 sudo resize2fs /dev/mapper/ubuntu--vg-ubuntu--lv
