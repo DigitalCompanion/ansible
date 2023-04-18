13837a64ffb1dd4dde4de847a456ad2de7dc06db
Fredrik Hansen <46652156+DigitalCompanion@users.noreply.github.com>
2023-04-18 13:28:47 +0200

    Updated with docker2 and removed duplicate lines

--- install_cuda.sh
+++ install_cuda.sh
@@ -1,8 +1,9 @@
 sudo wget -O /etc/apt/preferences.d/cuda-repository-pin-600 https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
 sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.0-1_all.deb 
-sudo dpkg -i cuda-keyring_1.0-1_all.deb
+### Install GPG keys
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.0-1_all.deb
+sudo dpkg -i cuda-keyring_1.0-1_all.debb
 
 sudo apt-key del 7fa2af80
 
@@ -10,6 +11,7 @@ sudo apt update
 
 sudo apt install cuda
 sudo apt install nvidia-cuda-toolkit
+
 echo 'export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}' >> ~/.bashrc
 
 nvcc --version
@@ -19,12 +21,11 @@ curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 
 sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit nvidia-container-runtime
+sudo apt install nvidia-docker2
 sudo systemctl restart docker
 
 
 docker run --gpus all nvidia/cuda:10.0-base nvidia-smi
 
 
-### Install GPG keys
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.0-1_all.deb
-sudo dpkg -i cuda-keyring_1.0-1_all.deb
+
