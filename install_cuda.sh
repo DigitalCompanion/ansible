b61ca6b6cc1d05294197306dd6c09b5de14acde6
Fredrik Hansen <46652156+DigitalCompanion@users.noreply.github.com>
2023-04-20 09:25:35 +0200

    Updated with docker-compose setup

--- install_cuda.sh
+++ install_cuda.sh
@@ -28,4 +28,10 @@ sudo systemctl restart docker
 docker run --gpus all nvidia/cuda:10.0-base nvidia-smi
 
 
+# install docker-compose using curl + grep
+VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | grep -Po '"tag_name": "\K.*\d')
+
+DESTINATION=/usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
+sudo chmod 755 $DESTINATION
 
